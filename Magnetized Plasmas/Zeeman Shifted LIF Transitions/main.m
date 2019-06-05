9ce6a012d5b070a5a47e16c535aa93f4ccca68f4
Grant Gorman <51376859+theCaffeinatedOwl22@users.noreply.github.com>
2019-06-05 13:28:54 -0500

    Restructured this code prior to redoing it

---
+++ Magnetized Plasmas/Zeeman Shifted LIF Transitions/main.m
@@ -0,0 +1,11 @@
+%% Program Notes
+% This program calculates the Zeeman shifts of the imaging transition for
+% the Sr ion. A write-up has been documented in the OneNote on 06/05/19
+
+%% Initiate Program
+clc
+clear
+close all
+
+%% User Inputs
+
