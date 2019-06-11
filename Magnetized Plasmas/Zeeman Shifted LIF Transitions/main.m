9e0700667bdeafae40c14f381c3823db21102920
Grant Gorman <51376859+theCaffeinatedOwl22@users.noreply.github.com>
2019-06-10 19:20:25 -0500

    Completed feature for plotting Voigt profiles
    The feature for plotting Voigt profiles is finished. The code is commented.

--- Magnetized Plasmas/Zeeman Shifted LIF Transitions/main.m
+++ Magnetized Plasmas/Zeeman Shifted LIF Transitions/main.m
@@ -1,37 +1,55 @@
 %% Program Notes
-% This program calculates the Zeeman shifts of the imaging transition for
-% the Sr ion. A write-up has been documented in the OneNote on 06/05/19
+% This program has several different features that calculate important
+% quantities related to using LIF imaging on a magnetized UNP. 
+
+% Options
+%   - Plot Zeeman shifts of LIF magnetic sublevels as a function of
+%   magnetic field
+%
+%   - Plot Zeeman-shifted Voigt profiles for user-specified locations from
+%   plasma center.
+
+
+% See OneNote entry for 06/05/19 for detailed discussion of imaging
+% magnetized plasmas.
 
 %% Initiate Program
+% Clear workspace
 clc
 clear
 close all
-
-%% User Inputs
-%%%   Program Options %%%
-flag.plotShiftVsField = 0;
-flag.plotSpectraVsLocation = 1;
 % Specify path location for sub programs
 addpath('C:\Users\grant\Documents\GitHub\Ultracold-Plasmas\Magnetized Plasmas\Zeeman Shifted LIF Transitions\subPrograms');
+
+%% User Inputs
+%%%   General Options   %%%
+flag.plotShiftVsField = 0;
+flag.plotSpectraVsLocation = 1;
+
 % Define Sr+ imaging transitions
 [t] = defineTransitions();
+
 % Define magnetic field
 [B] = defineMagneticField();
+
+%%%   Options for plotting Zeeman-shifted Voigt profiles   %%%
 % Specify which transitions to plot
-trans = [3 4]; % [PiMinus, PiPlus, SigPlus, SigMinus] specify indices
+trans = [1 2 3 4]; % [PiMinus, PiPlus, SigPlus, SigMinus] specify indices
+
 % Specify locations for which spectra are plotted
 loc.x = round(linspace(-5,5,7),2); % units = mm
 loc.y = zeros(size(loc.x)); % units = mm
+
 % Specify ion temperature
 Ti = 1; % units = K
 
 %% Plot Zeeman shifts as a function of magnetic field
 if flag.plotShiftVsField == 1
-    plotShiftVsField(t)
+    [f,ax] = plotShiftVsField(t);
 end
 
 %% Plot how we expect spectra to look
 if flag.plotSpectraVsLocation == 1
-    plotSpectraVsLocation(t(trans),B,loc,Ti)
+    [f,axSpec,ax,anX,anTitle,axLvlDiagram] = plotSpectraVsLocation(t(trans),B,loc,Ti);
 end
 
--- Magnetized Plasmas/Zeeman Shifted LIF Transitions/subPrograms/plotShiftVsField.m
+++ Magnetized Plasmas/Zeeman Shifted LIF Transitions/subPrograms/plotShiftVsField.m
@@ -1,32 +1,60 @@
-function [] = plotShiftVsField(t)
+function [f,ax] = plotShiftVsField(t)
+%% Program Notes
+% This program plots the Zeeman shifts of the Pi and Sigma transitions for
+% our LIF imaging on the D1 line of the Strontium ion (2S_1/2 - 2P_1/2) as
+% a function of magnetic field. 
+%
+% Inputs
+%   - t: (1xk structure) contains LIF transition information for k
+%         transitions
+%       t(k).name: (string) name of transition, used for legend
+%       t(k).upper: (1x5 double) |n l s j mj> quantum numbers for
+%                    upper level of transition
+%       t(k).lower: (1x5 double) |n l s j mj> quantum numbers for lower
+%                    level of transition
+%       t(k).color: (1x3 double) rgb triplet that defines color of line in
+%                    for the plot
+%
+% Outputs:
+%   - f: (figure object) figure object for plot of Zeeman shifts vs
+%         magnetic field
+%   - ax: (axes object) axes object for plot of Zeeman shifts vs magnetic
+%          field
+
 %%
 % Define transition Zeeman shifts as a function of magnetic field
+dEz = cell(size(t));
 for i = 1:size(t,2)
     dEz{i} = @(bField) calcAnomalousZeemanShift(t(i).upper,bField)-calcAnomalousZeemanShift(t(i).lower,bField);
 end
 % Define range of magnetic field for which shift will be plotted
-bField = [0:.02/100:.01];
+bField = 0:.02/100:.01;
 % Plot the shift vs field
+
+%% Plot Zeeman shifts vs magnetic field
+% Open new figure
 f = figure;
-% Set figure position
+% Set figure window size
 f.Units = 'inches';
 f.Position = [2 2 6 5];
+% Plot Zeeman shift for each transition within the same figure
 for i = 1:length(dEz)
-    plot(bField*1e4,dEz{i}(bField)*1e-6,'LineWidth',2);
+    plot(bField*1e4,dEz{i}(bField)*1e-6,'LineWidth',2,'Color',t(i).color);
     hold on
 end
+% Set default font size for figure
 ax = gca;
 ax.FontSize = 14;
 % Add legend to plot
-clear legendStr
+legendStr = cell(size(t));
 for i = 1:size(t,2)
     legendStr{i} = t(i).name;
 end
 lgd = legend(legendStr);
 lgd.Location = 'northwest';
 % Add other labels
-xlabel('B (G)')
-ylabel('\DeltaE_z (MHz)')
-title('Zeeman Shifts of Sr^+ LIF Transition')
+xlabel('B (G)');
+ylabel('\DeltaE_z (MHz)');
+title('Zeeman Shifts of Sr^+ LIF Transition');
 end
 
