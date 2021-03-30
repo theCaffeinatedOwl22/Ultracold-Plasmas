4b86e61d77a1dfca246629ee3c8c75f6eec57e3b
Grant Gorman <gg25@rice.edu>
2021-03-29 20:08:59 -0500

    update code examples

---
+++ code-examples/an_string.m
@@ -0,0 +1,6 @@
+dlm = '-';
+str1 = ['Phase' num2str(s2.phase)];
+str2 = ['Fields' num2str(s2.mag)];
+str3 = ['Te' num2str(s2.Te)];
+str4 = ['t' num2str(s2.t) 'us'];
+an.String = [str1 dlm str3 dlm str2 dlm str4 dlm '<' andir '>'];
---
+++ code-examples/textbox.m
@@ -0,0 +1,5 @@
+txt = text(0,0,'test','Units','normalized');
+txt.Rotation = 90;
+txt.HorizontalAlignment = 'center';
+txt.Position = [-0.3780    0.4498         0];
+txt.FontSize = 14;
