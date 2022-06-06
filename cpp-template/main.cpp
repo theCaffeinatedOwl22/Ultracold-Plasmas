f63c2f3672251a9e42c1844b04098576a9dc7b1d
Grant Gorman <gg25@rice.edu>
2022-06-05 21:52:46 -0500

    Create timer class

--- cpp-template/main.cpp
+++ cpp-template/main.cpp
@@ -2,17 +2,16 @@
 #include "utility.hpp"
 #include "matrix.hpp"
 #include "class_notes.hpp"
+#include "Timer.hpp"
 
 #include <iostream>
 #include <filesystem>
 
 int main(int argc, char *argv[])
 {   
-    ClassNotes::static_function();
-    ClassNotes test{2,2};
-    int x {5};
-    int* xptr {&x};
-    int& xref {*xptr};
-    std::cout << "testing" << std::endl;
+    Timer timer;
+    ClassNotes test;
+    std::cout << "Test complete." << std::endl;
+    timer.print_elapsed();
     return 0;
 }
--- cpp-template/source/class_notes.hpp
+++ cpp-template/source/class_notes.hpp
@@ -10,32 +10,32 @@ class ClassNotes
         
         // *** Constructors - have no return type, not even void
 
-        ClassNotes() = default; // default constructor - used for instantiation of a class with no provided values
-                                // only include this if other constructors do not have complete default values
-        ClassNotes(double val,double val2); // alternate constructor
+            ClassNotes(double val,double val2); // alternate constructor
+            ClassNotes(); // using a default constructor to delegate to an alternate
+            // ClassNotes() = default; // designate an empty default constructor when one does not exist
+
+        // *** Member Functions
+            double member() const;
+            void set_member(double val);
+            ClassNotes& chainable_func();
+
+        // *** Static Examples
+            static const inline std::string m_static_member {"test static..."}; // static means property of class, not class object
+                // only integral types can be defined inline without the inline keyword
+                // need to learn difference between const and constexpr
+            static const inline std::vector<int> m_lambda_ex {
+                []{
+                    std::vector<int> temp(4);
+                    for (int i=0; i<temp.size(); i++) temp[i]=i;
+                    return temp;
+                }() // evaluate lambda right away
+            }; // example of using lambda for initialization
+            static const std::string& static_function();
 
         // *** Friends
             // these are not member functions, so require passing of class into function
             // can access private members of class they are friend to
-        friend void friendly_printer(ClassNotes& obj); 
-
-        // *** Member Functions
-        double member() const;
-        void set_member(double val);
-        ClassNotes& chainable_func();
-
-        // *** Static Examples
-        static const inline std::string m_static_member {"test static..."}; // static means property of class, not class object
-            // only integral types can be defined inline without the inline keyword
-            // need to learn difference between const and constexpr
-        static const inline std::vector<int> m_lambda_ex {
-            []{
-                std::vector<int> temp(4);
-                for (int i=0; i<temp.size(); i++) temp[i]=i;
-                return temp;
-            }() // evaluate lambda right away
-        }; // example of using lambda for initialization
-        static const std::string& static_function();
+            friend void friendly_printer(ClassNotes& obj); 
 
     private:// all members and non-end-user functions
             // all  members are initialized in the order they are declared
@@ -43,7 +43,7 @@ class ClassNotes
     // *** Class Members
 
         double m_member {}; // use brace initialization to avoid undefined behavior
-        const double m_const_member {}; // must be initialized in constructor
+        const double m_const_member {}; // must be initialized in constructor because const
         
 
     // *** Private Functions
