59f19baaf26580cd2de04c123def46e29b3f6c21
Grant Gorman <gg25@rice.edu>
2022-06-15 22:15:48 -0500

    Created Makefile, but does not work

--- cpp-template/main.cpp
+++ cpp-template/main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
+#include <armadillo>
 
 int main(int argc, char *argv[])
 {   
+    std::cout << "Complete." << std::endl;
     std::cout << "Complete." << std::endl;
     return 0;
 }
