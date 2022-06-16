6d432a5fd7e5c5e3111e9e32065cfeabc8f7ba55
Grant Gorman <gg25@rice.edu>
2022-06-16 15:15:11 -0500

    Created working Makefile

--- cpp-template/main.cpp
+++ cpp-template/main.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 #include <armadillo>
-
+#include <matrix.hpp>
+#include <utility.hpp>
 int main(int argc, char *argv[])
 {   
-    std::cout << "Complete." << std::endl;
     std::cout << "Complete." << std::endl;
     return 0;
 }
