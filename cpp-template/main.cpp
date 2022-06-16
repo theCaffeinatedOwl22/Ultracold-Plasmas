ac53a9a8cc474978e658305431fe28421103d8c9
Grant Gorman <gg25@rice.edu>
2022-06-15 21:30:35 -0500

    Clean up repo

--- cpp-template/main.cpp
+++ cpp-template/main.cpp
@@ -1,20 +1,7 @@
-#include "settings.hpp"
-#include "utility.hpp"
-#include "Matrix.hpp"
-#include "Timer.hpp"
-
 #include <iostream>
-#include <filesystem>
-
 
 int main(int argc, char *argv[])
 {   
-    Timer timer;
-    Matrix test(1,4,1);
-    test(3)=2;
-    test = test.t();
-
-
     std::cout << "Complete." << std::endl;
     return 0;
 }
