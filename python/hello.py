dded0ae8aebe6cb3bd32b07623aef6bf69ded6d5
Grant Gorman <gg25@rice.edu>
2023-03-13 13:57:48 -0500

    Add batch script for installing Python packages

--- python/hello.py
+++ python/hello.py
@@ -1,1 +1,2 @@
-print("Hello world")
+msg = "Hello World"
+print(msg)
---
+++ python/packages.bat
@@ -0,0 +1,7 @@
+:: Packages to be installed in virtual environment
+
+:: Before Installing:
+:: - Create virtual environment with <py -3 -m venv .venv>
+:: - Activate virtual environment with <.venv\scripts\activate>
+
+python -m pip install matplotlib
