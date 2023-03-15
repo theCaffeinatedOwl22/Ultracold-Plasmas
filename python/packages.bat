f8b27ee5a831ae809c9efd0dcbfba73bcb2d5b2b
Grant Gorman <gg25@rice.edu>
2023-03-15 11:35:08 -0500

    Create requirements.txt

--- python/packages.bat
+++ python/packages.bat
@@ -1,7 +1,10 @@
 :: Packages to be installed in virtual environment
 
 :: Before Installing:
-:: - Create virtual environment with <py -3 -m venv .venv>
+:: - Create virtual environment with <python -m venv .venv>
 :: - Activate virtual environment with <.venv\scripts\activate>
+:: - Run this .bat file or install requirements.txt with <pip install -r requirements.txt>
+:: - Create requirements.txt file using <pip freeze > requirements.txt>
 
+python -m pip install numpy
 python -m pip install matplotlib
---
+++ python/scratch.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import math as m
+import os
+import subprocess
+
+test_list = [1,'2',4.5]
+test_string = str(test_list)
+test_string_2 = [*test_list]
+print(test_list[0:2])
