1ff2c7a9f7336133227cfb4ba3957841502b1beb
Grant Gorman <gg25@rice.edu>
2023-03-15 12:34:07 -0500

    Update python folder to be a poetry folder

--- python/scripts/standardplot.py
+++ python/scripts/standardplot.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import math as m
 
+
 x = np.linspace(0, m.pi, 100)  # Create a list of evenly-spaced numbers over the range
 plt.plot(x, np.sin(x))       # Plot the sine of each x point
 plt.show()                   # Display the plot
