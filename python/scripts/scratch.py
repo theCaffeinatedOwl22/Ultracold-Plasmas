3ce1196dd87fd4d29a488f0bbc299947f3b36a16
Grant Gorman <gg25@rice.edu>
2023-03-17 13:53:10 -0500

    Install scipy

--- python/scripts/scratch.py
+++ python/scripts/scratch.py
@@ -4,7 +4,7 @@ import math as m
 import os
 import subprocess
 
-test_list = [1,'2',4.5]
+test_list = [1, '2', 4.5]
 test_string = str(test_list)
 test_string_2 = [*test_list]
 print(test_list[0:2])
